48c0f444a3573eb4afb52702776c2cd9a9c9d469
Peeradon <peeradon.dora@gmail.com>
2026-04-08 18:22:20 +0700

    Change a little bit in Taskmanager.py

--- Taskmanager.py
+++ Taskmanager.py
@@ -1,20 +1,31 @@
 import feature as ft
-print("Task Manager")
-print("""1 Add Task
+while True:
+    try:    
+        print("Task Manager")
+        print("""1 Add Task
 2 Delete Task
 3 Show All Task
-4 Show Task Status""")
-choose=int(input("Option Select (1,2,3,4) : "))
-match choose:
-    case 1:
-        print("--------Add Task--------")
-        ft.addtask()
-    case 2:
-        print("--------Delete Task--------")
-        ft.deletetask()
-    case 3:
-        print("--------Show All Task--------")
-        ft.showtask()
-    case 4:
-        print("--------Commit Task--------")
-        ft.taskcommit()
+4 Show Task Status5
+5 Exit""")
+        choose=int(input("Option Select (1,2,3,4,5) : "))
+        match choose:
+            case 1:
+                print("--------Add Task--------")
+                ft.addtask()
+            case 2:
+                print("--------Delete Task--------")
+                ft.deletetask()
+            case 3:
+                print("--------Show All Task--------")
+                ft.showtask()
+            case 4:
+                print("--------Commit Task--------")
+                ft.taskcommit()
+            case 5:
+                print("--------Exit--------")
+                break
+
+    except ValueError:
+        print("---------------------------------------")
+        print("There is no option in this task manager")
+        print("---------------------------------------")
