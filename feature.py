b29f674a8ca889a17dc22eda23225c8304407106
Peeradon <peeradon.dora@gmail.com>
2026-04-08 18:20:36 +0700

    Improve some code for user experienced change data type into dict for easy data accessing in feature .py

--- feature.py
+++ feature.py
@@ -1,37 +1,53 @@
 import json
 from os import path
-task={}
-def addtask():
-    task_name=input("Task Name : ")
-    task_des =input(f"{task_name} Description :")
-    task["name"]=task_name
-    task["description"]=task_des
-    task["status"]=False
-    if not path.exists("D:/Github Repo/Python-Task-Manager/task.json"):
-        user=[]
-        user.append(task)
-        with open("D:/Github Repo/Python-Task-Manager/task.json",'a') as f:
-            json.dump(user,f,indent=2)
-    else:
-        with open("D:/Github Repo/Python-Task-Manager/task.json") as f:
-            data=json.load(f)
-        data.append(task)
-        print(data)
-        with open ("D:/Github Repo/Python-Task-Manager/task.json",'w') as f:
-            json.dump(data,f,indent=2)
 
+
+def addtask():
+    task={}
+    
+    #ใส่ข้อมูล task ที่จะเพิ่ม ใน dict
+    task_name=input("Task Name : ")
+    task_des =input(f"{task_name} Description : ")
+    task["name"]=task_name          #task.update({"name":task_name})
+    task["description"]=task_des    #task.update({"description":task_des})
+    task["completed"]=False            #task.update({"completed":False})
+    print("Task Added...")
+
+    if not path.exists("D:/Github Repo/Python-Task-Manager/task.json"):   #กรณี file ยังไม่เคยถูกสร้าง (ไม่เคยใช้)
+        user={}
+        user.update({task_name:task})           #<-- เอา task เพิ่มเข้าไปใน list/array
+        with open("D:/Github Repo/Python-Task-Manager/task.json",'a') as f:
+            json.dump(user,f,indent=2)          #<-- แปลงและเก็บข้อมูลให้เป็น Json object+array
+            """ข้อมูลถูกเก็บใน Json file แล้ว"""
+    
+    else: #กรณีที่ file ถูกสร้างแล้ว (เคยใช้)
+        with open("D:/Github Repo/Python-Task-Manager/task.json") as f:
+            data=json.load(f)       #<-- ดึงข้อมูลใน Json มาเก็บไว้ใน Data
+        data.update({task_name:task})     #update ข้อมูลใหม่ใน list Data
+
+        with open ("D:/Github Repo/Python-Task-Manager/task.json",'w') as f:
+            json.dump(data,f,indent=2) 
+            """Rewrite Json file ด้วยข้อมูลที่ update แล้ว"""
+"""--------------------------------------------------------------------------------------------------"""
 def deletetask():
     pass
 
 def showtask():
+    count=0
+
     with open("D:/Github Repo/Python-Task-Manager/task.json") as f:
-        data=json.load(f)
-        for task in data:
-            print(f"""name: {task["name"]}
+        data=json.load(f)   #<-- ดึงข้อมูลใน Json มาเก็บไว้ใน Data
+    
+    for task in data.values(): #show ข้อมูล ทั้งหมด
+        count+=1
+        print(f"No. {count}")
+        print(f"""name: {task["name"]}
 description: {task["description"]}
-status: {task["status"]}""")
-            print('--------------------------------------------------')
-                 
+completed: {task["completed"]}""")
+        print('--------------------------------------------------')
+    count=0 
 
 def taskcommit():
-    pass
+    with open("D:/Github Repo/Python-Task-Manager/task.json") as f:
+        data=json.load(f)   #<-- ดึงข้อมูลใน Json มาเก็บไว้ใน Data
+    
