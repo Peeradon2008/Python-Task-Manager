5d67df5bca4b8e8cb925dcde529ec18031394363
Peeradon <peeradon.dora@gmail.com>
2026-04-09 19:14:45 +0700

    Add filefound check check if file not found ypu can use only 1.add task feature if file found you can use all feature

--- Taskmanager.py
+++ Taskmanager.py
@@ -1,35 +1,49 @@
 import feature as ft
+from json import load
+from os import path
 while True:
     try:    
-        print("Task Manager")
-        print("""1 Add Task
+        file_found=path.exists("D:/Github Repo/Python-Task-Manager/task.json")
+        print("---------Task Manager---------")
+        if file_found:
+            print("""  -------Choose Option-------
+1 Add Task
 2 Delete Task
 3 Show All Task
 4 Commit Task
 5 Edit Task
 6 Exit""")
-        choose=int(input("Option Select (1,2,3,4,5) : "))
-        match choose:
-            case 1:
+            with open("D:/Github Repo/Python-Task-Manager/task.json") as f:
+                data = load(f)
+        else:
+            data=None
+            print("""Choose Option
+1 Add Task
+or Type Exit""")
+        choose=(input("Option Select (Number or Exit) : "))
+        match choose,file_found:
+            case '1',True|False:
                 print("--------Add Task--------")
-                ft.addtask()
-            case 2:
+                ft.addtask(data,file_found)
+            case '2',True: 
                 print("--------Delete Task--------")
-                ft.deletetask()
-            case 3:
+                ft.deletetask(data)
+            case '3',True:
                 print("--------Show All Task--------")
-                ft.showtask()
-            case 4:
+                ft.showtask(data)
+            case '4',True:
                 print("--------Commit Task--------")
-                ft.taskcommit()
-            case 5:
+                ft.taskcommit(data)
+            case '5',True:
                 print("--------Edit Task--------")
-                ft.taskedit()
-            case 6:
+                ft.taskedit(data)
+            case '6' | "Exit" | "exit",True|False:
                 print("--------Exit--------")
                 break
+            case _:
+                raise Exception
 
-    except ValueError:
+    except ZeroDivisionError:
         print("---------------------------------------")
         print("No that option in this task manager")
         print("---------------------------------------")
