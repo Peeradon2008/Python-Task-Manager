9113308199f722acaac769dcad4f95a2d65281fc
Peeradon <peeradon.dora@gmail.com>
2026-04-10 18:42:47 +0700

    store file path in variable and class

--- exception.py
+++ exception.py
@@ -2,3 +2,10 @@ class OptionError(Exception):
     pass
 class TaskNotFoundError(Exception):
     pass
+class filePath:
+    def __init__(self):
+        self._filepath = "D:/Github Repo/Python-Task-Manager/task.json"
+    def filepathchange(self,new_path):
+        self._filepath = new_path
+    def showpath(self):
+        print(f"Your File path -> {self._filepath}")
