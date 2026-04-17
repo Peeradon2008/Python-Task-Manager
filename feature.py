ea38e99880e0f893451f3c9c5b17f68051e95664
Peeradon <peeradon.dora@gmail.com>
2026-04-17 18:49:53 +0700

    Complete class Feature for Taskmanager

--- feature.py
+++ feature.py
@@ -1,47 +1,90 @@
 import json
 import exception as ex
 json_file="D:/Github Repo/Python-Task-Manager/task.json"
-class Addtask:
-    def __init__(self,data:dict,found:bool):
-        self.__name = input("Task name : ")
-        self.__data = data
-        self.__task = self.__input() 
-        if found:
-            self.__found()
-        else:
-            self.__notfound()
-    
-    def __input(self):
-        task={}
-        task_des = input(f"{self.__name} Description : ")
-        task["name"]=self.__name  #task.updated({"name":task_name})
-        task["description"]=task_des #task.updated({"description":task_des})
-        task["completed"]=False  #task.updated({"completed":False})
-        return task
-    
-    def __found(self):
-        self.__data.update({self.__name:self.__task})
-        self.writejsonfile()
-    
-    def __notfound(self):
-        user={}
-        user.update({self.__name:self.__task})
-        with open(json_file,'a') as f:
-            json.dump(user,f,indent=2)
-    
-    def writejsonfile(self):
+
+class Feature:
+    def __init__(self, task_name):
+        with open(json_file) as f:
+            self.__data = json.load(f)
+        self.__name = task_name
+
+    def savetask (self):
         with open(json_file,'w') as f:
             json.dump(self.__data,f,indent=2)
 
-class Deletetask(Addtask):
-    def __init__(self,data:dict):
-        self.__data = data
-        self.__name = input("Task name : ")
-        if self.__name in self.__data:
-            self.__delete
-        else:
+    def addtask(self):
+        task={}
+        task["name"]=self.__name
+        task["description"]=input(f"{self.__name} description : ")
+        task["completed"]=False
+        self.__data.update({self.__name:task})
+        self.savetask()
+        print("Task Added...\n")
+    
+    def deletetask(self):
+        if len(self.__data) == 0:
             raise ex.TaskNotFoundError
-    def __delete(self):
-        del self.__data[self.__name]
-    def writejsonfile(self):
-        super().writejsonfile()
+        else:
+            if self.__name in self.__data:
+                del self.__data[self.__name]
+                self.savetask()
+                print("Task Deleted...\n")
+            elif self.__name not in self.__data:
+                raise ex.TaskDeleteError
+
+    def taskcommit(self):
+        if len(self.__data) == 0:
+            raise ex.TaskNotFoundError
+        else:
+            if self.__name in self.__data:
+                self.__data[self.__name].update({"completed":True})
+                self.savetask()
+                print("Task Commit...\n")
+            elif self.__name not in self.__data:
+                raise ex.TaskCommitError
+
+    def searchtask(self):
+        if len(self.__data) == 0:
+            raise ex.TaskNotFoundError
+        else:
+            if self.__name in self.__data:
+                searched = self.__data[self.__name]
+                print(f"""----------------------------------------
+name : {searched["name"]}
+description : {searched["description"]}
+completed : {searched["completed"]}
+----------------------------------------""")
+            elif self.__name not in self.__data:
+                raise ex.TaskSearchError
+            
+    
+    def taskedit(self):
+        if len(self.__data) == 0:
+            raise ex.TaskeditError
+        else:
+            if self.__name in self.__data:
+                print("""Choose what you want to edit
+1 name
+2 description
+3 status""")
+                edit=int(input("Edit Option : "))
+                match edit:
+                    case 1:
+                        new_name = input("new name : ")
+                        self.__data[self.__name].update({"name":new_name})
+                        self.__data[new_name]=self.__data.pop(self.__name)
+
+                        self.__name = new_name
+                    case 2:
+                        new_des = input("new descriotion : ")
+                        self.__data[self.__name].update({"description":new_des})
+                    
+                    case 3:self.__data[self.__name].update({"completed":False})
+                self.savetask()
+                print("Task Edited...\n")
+            elif self.__name not in self.__data:
+                raise ex.TaskNotFoundError
+            
+        
+
+#----------------------------------End Class---------------------------------------#
