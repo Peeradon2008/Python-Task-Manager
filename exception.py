56ace32b2b3f458d8304dfbcd87484425443f5d9
Peeradon <peeradon.dora@gmail.com>
2026-04-20 18:08:20 +0700

    Add more Custom Exception class

--- exception.py
+++ exception.py
@@ -1,11 +1,12 @@
 class OptionError(Exception):
     pass
+class TaskDeleteError(Exception):
+    pass
+class TaskCommitError(Exception):
+    pass
+class TaskSearchError(Exception):
+    pass
+class TaskeditError(Exception):
+    pass
 class TaskNotFoundError(Exception):
     pass
-class filePath:
-    def __init__(self):
-        self._filepath = "D:/Github Repo/Python-Task-Manager/task.json"
-    def filepathchange(self,new_path):
-        self._filepath = new_path
-    def showpath(self):
-        print(f"Your File path -> {self._filepath}")
