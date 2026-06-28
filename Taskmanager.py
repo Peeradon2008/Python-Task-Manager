8f95e799d33adb4a14124ea172c1640187747e53
Peeradon <peeradon.dora@gmail.com>
2026-06-28 14:42:43 +0700

    Enter new blank line

--- Taskmanager.py
+++ Taskmanager.py
@@ -1,12 +1,14 @@
 import frontend as fr
 import exception as ex
+
 while True: 
     try:
         print("---------Task Manager---------")
         fr.showText()
         option_selected = input("Option Select (Number or Type Exit) : ")
         feature_option=fr.inputOption(option_selected)
-        if feature_option==7:
+
+        if feature_option==7: #7 is exitcode
             print("------------Exit Program------------")
             break
         else:
