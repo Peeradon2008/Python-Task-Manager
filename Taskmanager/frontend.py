6c9ca46062141a9ceda33d9577b751cfec49ab6b
Peeradon <peeradon.dora@gmail.com>
2026-06-29 23:04:09 +0700

    Decrease code complexity -Add getData function for tracking code workflow easily -Delete inputOption function for decrease complexity -edit callFeature function in each case call the Feature class directly and return exitcode in each case

--- Taskmanager/frontend.py
+++ Taskmanager/frontend.py
@@ -2,6 +2,10 @@
 import feature as ft
 import exception as ex
 
+def getData ():
+    data=ft.Accessjson().access()
+    return data
+
 def showText(data_length): #show text
 
     # only use Addtask unless task data have stored
@@ -19,48 +23,51 @@ def showText(data_length): #show text
 5 Commit Task
 6 Edit Task
 7 Exit""")
-
-def inputOption(option_selected): #choose option to create object from feature class
-    
-    match option_selected:
-        case '1'|'2'|'3'|'5'|'6': #1) this option require taskname
-            feature = ft.Feature(input("Task name : ")) #input taskname
-            return feature
-        
-        case '4': #2) not require taskname
-            feature = ft.Feature(None)
-            return feature
-        
-        case '7'|"exit"|"Exit"|"EXIT": #3) exit
-            return 7 #return 7 exit code
-        
-        case _: raise ex.OptionError(f"No > {option_selected} < option in this task manager")
-
-def callFeature(option_selected,data_length,feature): #use task feature
+            
+def callFeature(option_selected,data_length): #use task feature
 
     match option_selected,data_length:
         case '1',x if x>=0: 
             print("--------Add Task--------")
-            feature.addtask()
+            ft.Feature(input("Task name : ")).addtask()
+            exitcode=False
+            return exitcode
         
         case '2',x if x>0: #2-6 can use only unblank json file
             print("--------Delete Task--------")
-            feature.deletetask()
+            ft.Feature(input("Task name : ")).deletetask()
+            exitcode=False
+            return exitcode
         
         case '3',x if x>0:
             print("--------Search Task--------")
-            feature.searchtask()
+            ft.Feature(input("Task name : ")).searchtask()
+            exitcode=False
+            return exitcode
+
         case '4',x if x>0:
             print("--------Show All Task--------")
-            feature.showalltask()
+            ft.Feature("").showalltask()
+            exitcode=False
+            return exitcode
+        
         case '5',x if x>0:
             print("--------Commit Task--------")
-            feature.taskcommit()
+            ft.Feature(input("Task name : ")).taskcommit()
+            exitcode=False
+            return exitcode
         
         case '6',x if x>0:
             print("--------Edit Task--------")
-            feature.taskedit()
+            ft.Feature(input("Task name : ")).taskedit()
+            exitcode=False
+            return exitcode
+        
+        case '7'|"exit"|"Exit"|"EXIT",x if x>=0:
+            exitcode=True
+            return exitcode #return exitcode True -> break loop
 
         case _: raise ex.OptionError(f"No > {option_selected} < option in this task manager")
+
 
 #----------------------------------End Function---------------------------------------#
